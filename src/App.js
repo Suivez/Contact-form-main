61a8b35f7ce85f8d472ea5358e34d2d3e6e26c3c
Suivez <suivez@interia.pl>
2024-07-03 19:51:59 +0200

    03.07.2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 
 function App() {
+  const inputComponent = (labelName, type) => {
+    return(
+      <div className='box-input'>
+        <div className='label'>{labelName} <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
+        <input type={type}></input>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <div className='form-main'>
         <h2>Contact Us</h2>
         <div className='form-section'>
-          <div className='box-input'>
-            <div className='label'>First Name <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
-            <input type='text'></input>
-          </div>
-          <div className='box-input'>
-            <div className='label'>Last Name *</div>
-            <input type='text'></input>
-          </div>
+          {inputComponent("First Name", "text")}
+          {inputComponent("Last Name", "text")}
+        </div>
+        <div className='form-section'>
+          {inputComponent("Email Address", "Email")}
         </div>
       </div>
     </div>
