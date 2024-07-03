d5f148b61f6906e6894ef08bbe98e41dc2350251
Suivez <suivez@interia.pl>
2024-07-03 13:26:06 +0200

    03.07

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
     <div className="App">
       <div className='form-main'>
         <h2>Contact Us</h2>
+        <div className='form-section'>
+          <div className='box-input'>
+            <div className='label'>First Name <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
+            <input type='text'></input>
+          </div>
+          <div className='box-input'>
+            <div className='label'>Last Name *</div>
+            <input type='text'></input>
+          </div>
+        </div>
       </div>
     </div>
   );
