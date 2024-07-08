b3d892a56310808ffc7bd0d0cc53158b98d103fc
Suivez <suivez@interia.pl>
2024-07-08 17:51:50 +0200

    08.07.2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
 import './App.css';
 
 function App() {
-  const inputComponent = (labelName, type, name, labelRadioName1, labelRadioName2) => {
+  const inputComponent = (labelName, type) => {
     if(type === "radio") {
-      return(
-        <div className='box-input'>
-          <div className='label'>{labelName} <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
-          <div className='radio-box'>
-            <div className='radio-option'>
-              <input type={type} id={labelRadioName1} name={name}></input>
-              <label for={labelRadioName1}>{labelRadioName1}</label>
-            </div>
-            <div className='radio-option'>
-              <input type={type} id={labelRadioName2} name={name}></input>
-              <label for={labelRadioName2}>{labelRadioName2}</label>
-            </div>
+      return (
+        <div className='radio-box'>
+          <div className='radio-option'>
+            <input type={type} id={labelName} name="choice"></input>
+            <label for={labelName}>{labelName}</label>
           </div>
         </div>
-      );
+      )
     }
     else {
-      return(
-        <div className='box-input'>
-          <div className='label'>{labelName} <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
-          <input type={type} name={name}></input>
-        </div>
+      return (
+        <input type={type}></input>
       );
     }
   }
+
+  const inputSection = (titleName, type, labelName) => {
+      return(
+        <div className='box-input'>
+          <div className='label'>{titleName} <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
+          {inputComponent(labelName, type)}
+        </div>
+      );
+    }
 
   return (
     <div className="App">
       <div className='form-main'>
         <h2>Contact Us</h2>
         <div className='form-section'>
-          {inputComponent("First Name", "text", "first_name")}
-          {inputComponent("Last Name", "text", "last_name")}
+          {inputSection("First Name", "text")}
+          {inputSection("Last Name", "text")}
         </div>
         <div className='form-section'>
-          {inputComponent("Email Address", "email", "email_address")}
+          {inputSection("Email Address", "email")}
         </div>
         <div className='form-section'>
-          {inputComponent("Query Type", "radio", "chioce", "General Enquiry", "Support Request")}
+          {inputSection("Query Type", "radio", "General Enquiry")}
+          {inputSection("Query Type", "radio", "Support Request")}
           {/* <div className='label'>Query Type <span style={{color: "hsl(169, 82%, 27%)"}}>*</span></div>
           <div className='radio-box'>
             <input type='radio' id="general_enquiry" name='choice'/>
